5f50c7074d6c0c89231f4464447e65cb4fa134c5
Pavan Krishna Nimmagadda <pavan.krishna@appdynamics.com>
2018-04-17 14:12:08 -0700

    added appdynamics documentation

--- docs-book/output/master_middleman/config.rb
+++ docs-book/output/master_middleman/config.rb
@@ -1,7 +1,11 @@
 require 'bookbinder_helpers'
+require 'proof'
 require 'middleman-syntax'
 require 'middleman-livereload'
+require 'middleman-sprockets'
 require 'subdirectory_aware_assets'
+require 'middleman-compass'
+require 'font-awesome-sass'
 
 config = YAML.load_file('bookbinder_config.yml')
 config.each do |k, v|
@@ -13,7 +17,9 @@ set :markdown, :layout_engine => :erb,
                :tables => true,
                :autolink => true,
                :smartypants => true,
-               :fenced_code_blocks => true
+               :fenced_code_blocks => true,
+               :with_toc_data => true,
+               :no_intra_emphasis => true
 
 set :css_dir, 'stylesheets'
 
@@ -25,10 +31,15 @@ set :relative_links, false
 
 page '/owners.json', :layout => false
 
-activate :subdirectory_aware_assets
+activate :bookbinder
 
-activate :navigation
+activate :proof
 
 activate :syntax
 
 activate :livereload
+
+activate :sprockets
+import_path FontAwesome::Sass.fonts_path
+
+activate :subdirectory_aware_assets
